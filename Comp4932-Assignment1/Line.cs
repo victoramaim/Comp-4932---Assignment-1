9ac934949932167b6b226e5613fe74f2ca694fd4
victoramaim <vcarvalhodiasamaimva@my.bcit.ca>
2024-01-15 19:07:41 -0800

    First commit

---
+++ Comp4932-Assignment1/Line.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Comp4932_Assignment1
+{
+    public class Line
+    {
+        public Point start;
+        public Point end;
+        
+
+        // Constructs the line to use both start and end points
+        public Line(Point start) { 
+            this.start = start;
+            this.end.X = 0;
+            this.end.Y = 0;
+        }
+
+        
+        Pen pen = new Pen(Color.Black, 5); // Create the pen
+
+        public void setEnd(Point end)
+        {
+            this.end = end;
+        }
+        
+        // Draws the line between the two points
+        public void CreateLine (PaintEventArgs e)
+        {
+            
+            e.Graphics.DrawLine(pen, start, end);
+        }
+    }
+}
---
+++ Comp4932-Assignment1/SourceImage.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Comp4932_Assignment1
+{
+    partial class SourceImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SourceImage));
+            toolStrip1 = new ToolStrip();
+            toolStripDropDownButton1 = new ToolStripDropDownButton();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripDropDownButton1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(482, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            toolStrip1.ItemClicked += toolStrip1_ItemClicked;
+            // 
+            // toolStripDropDownButton1
+            // 
+            toolStripDropDownButton1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripDropDownButton1.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem });
+            toolStripDropDownButton1.Image = (Image)resources.GetObject("toolStripDropDownButton1.Image");
+            toolStripDropDownButton1.ImageTransparentColor = Color.Magenta;
+            toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            toolStripDropDownButton1.Size = new Size(46, 24);
+            toolStripDropDownButton1.Text = "File";
+            toolStripDropDownButton1.Click += toolStripDropDownButton1_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(128, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // SourceImage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 453);
+            Controls.Add(toolStrip1);
+            Location = new Point(165, 320);
+            Name = "SourceImage";
+            StartPosition = FormStartPosition.Manual;
+            Text = "Source image";
+            Load += SourceImage_Load;
+            MouseDown += Source_MouseDown;
+            MouseMove += Source_MouseMove;
+            MouseUp += Source_MouseUp;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripDropDownButton toolStripDropDownButton1;
+        private ToolStripMenuItem openToolStripMenuItem;
+    }
+}
